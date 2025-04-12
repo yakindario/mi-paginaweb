b2aaa8766d2cdbdeb36c7ef95857f688e0e7284c
Yakin Dario <yakin_d.m_15@hotmail.com>
2025-04-11 18:07:22 -0600

    Agregar integración de sitemap en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yakindario.com',
@@ -12,5 +14,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
